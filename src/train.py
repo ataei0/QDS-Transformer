45a1b2afaf69dd51b73d339fb163fd16422a3f6e
ataei0 <66563639+ataei0@users.noreply.github.com>
2022-10-28 13:19:31 -0700

    Update train.py

--- src/train.py
+++ src/train.py
@@ -1,35 +1,9 @@
-#!/usr/bin/env python3
-from absl import app, flags
-import logging
-import numpy as np
-import random
-import os
-import sys
-import yaml
-
-import torch
-
-import utils
-import experiment
-
-utils.handle_flags()
-
-
-
-def main(argv):    
-    logging.getLogger("transformers.tokenization_utils").setLevel(logging.ERROR)
-    FLAGS = flags.FLAGS
-    utils.print_flags(FLAGS)
-
-    # Random seed initialization.
-    random.seed(FLAGS.random_seed)
-    np.random.seed(FLAGS.random_seed)
-    torch.manual_seed(FLAGS.random_seed)
+torch.manual_seed(FLAGS.random_seed)
     # Configuration and paths.
-    cfg = yaml.load(open(FLAGS.config, 'r'), Loader=yaml.BaseLoader)
-    PATH_DATA = cfg['path_data'] 
-    PATH_CORPUS = '{}/{}'.format(PATH_DATA, cfg['corpus'])
-    PATH_DATA_PREFIX = '{}/{}'.format(PATH_DATA, cfg['data_prefix'])
+    cfg = yaml.load(open('/content/QDS-Transformer/src/config.yml', 'r'), Loader=yaml.BaseLoader)
+    #PATH_DATA = cfg['path_data'] 
+    #PATH_CORPUS = '{}/{}'.format(PATH_DATA, cfg['corpus'])
+    #PATH_DATA_PREFIX = '{}/{}'.format(PATH_DATA, cfg['data_prefix'])
     PATH_MODEL_PREFIX = '{}/{}'.format(cfg['path_model'], FLAGS.model_prefix)
     os.makedirs(PATH_MODEL_PREFIX, exist_ok=True)
 
@@ -41,11 +15,20 @@ def main(argv):
         layer.attention.self.attention_window = FLAGS.window_size
 
     # Load the corpus.
-    corpus = utils.Corpus(PATH_CORPUS, FLAGS)
+    import json
+    with open('/content/multidoc2dial/multidoc2dial_doc.json', 'r') as f:
+        multidoc2dial_doc = json.load(f)
+    corpus = utils.Corpus(multidoc2dial_doc, FLAGS)
     # Load train/dev data.
-    train_data = utils.Data(PATH_DATA_PREFIX + 'train', corpus, FLAGS)
-    dev_data = utils.Data(PATH_DATA_PREFIX + 'dev', corpus, FLAGS)
-    test_data = utils.Data(PATH_DATA_PREFIX + 'test', corpus, FLAGS)
+    with open('/content/multidoc2dial/multidoc2dial_dial_test.json', 'r') as f:
+        multidoc2dial_dial_test = json.load(f)
+    with open('/content/multidoc2dial/multidoc2dial_dial_train.json', 'r') as f:
+        multidoc2dial_dial_train = json.load(f)
+    with open('/content/multidoc2dial/multidoc2dial_dial_validation.json', 'r') as f:
+        multidoc2dial_dial_validation = json.load(f)        
+    train_data = utils.Data(multidoc2dial_dial_train, corpus, FLAGS)
+    dev_data = utils.Data(multidoc2dial_dial_validation, corpus, FLAGS)
+    test_data = utils.Data(multidoc2dial_dial_test, corpus, FLAGS)
     
     for epoch in range(FLAGS.last_epoch, FLAGS.num_epochs):
         print('Epoch {}'.format(epoch + 1), file=sys.stderr)
@@ -62,7 +45,7 @@ def main(argv):
         exp.dump_model(PATH_MODEL_PREFIX, str(epoch + 1))
  
         # Evaluate dev data.
-        test_eval = exp.eval_dump(test_data, FLAGS.num_sample_eval,
+        test_eval = exp.eval_dump(dev_data, FLAGS.num_sample_eval,
                 'Evaluating test queries')
         print('Test Evaluation', test_eval, file=sys.stderr)
 
@@ -78,4 +61,3 @@ def main(argv):
 
 if __name__ == '__main__':
     app.run(main)
-
