c57f2381df81f06e26db0efce2a492d1613715ed
ataei0 <66563639+ataei0@users.noreply.github.com>
2022-10-28 14:04:17 -0700

    Update train.py

--- src/train.py
+++ src/train.py
@@ -1,4 +1,30 @@
-torch.manual_seed(FLAGS.random_seed)
+#!/usr/bin/env python3
+from absl import app, flags
+import logging
+import numpy as np
+import random
+import os
+import sys
+import yaml
+
+import torch
+
+import utils
+import experiment
+
+utils.handle_flags()
+
+
+
+def main(argv):    
+    logging.getLogger("transformers.tokenization_utils").setLevel(logging.ERROR)
+    FLAGS = flags.FLAGS
+    utils.print_flags(FLAGS)
+
+    # Random seed initialization.
+    random.seed(FLAGS.random_seed)
+    np.random.seed(FLAGS.random_seed)
+    torch.manual_seed(FLAGS.random_seed)
     # Configuration and paths.
     cfg = yaml.load(open('/content/QDS-Transformer/src/config.yml', 'r'), Loader=yaml.BaseLoader)
     #PATH_DATA = cfg['path_data'] 
